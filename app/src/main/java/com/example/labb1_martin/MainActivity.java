1ac0da4abe591b4c6e0a9caafbfde3cd64b892ea
LocalTweekez <serhad14@live.se>
2023-11-14 12:57:27 +0100

    mainactivity

--- app/src/main/java/com/example/labb1_martin/MainActivity.java
+++ app/src/main/java/com/example/labb1_martin/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayApp() {
         initialView();
         SetValues();
         refresh.setOnClickListener(view -> {SetValues();});
