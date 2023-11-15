7abe1a8c6d3a975e4f39965f3cc522b6be9cbfcd
amarildo <amarildo.rajta@gmail.com>
2023-11-15 11:54:29 +0100

    add working getters for precipitation, cloudiness. add weather image url getter, getWeatherImgURL()

--- app/src/main/java/com/example/labb1_martin/WeatherData.java
+++ app/src/main/java/com/example/labb1_martin/WeatherData.java
@@ -1,13 +1,18 @@
 package com.example.labb1_martin;
 
+/*
+ * This class represents weather data.
+ */
 public class WeatherData {
-    public WeatherData(double temperature, String weatherCondition, String windSpeed, String windDirection, String humidity, String precipitation) {
+    public WeatherData(double temperature, String weatherCondition, String windSpeed, String windDirection, String humidity, Double precipitation, String cloudiness) {
         this.temperature = temperature;
         this.weatherCondition = weatherCondition;
         this.windSpeed = windSpeed;
         this.windDirection = windDirection;
         this.precipitation = precipitation;
         this.humidity = humidity;
+        this.cloudiness = cloudiness;
+        this.weatherConditionImgURL = weatherIconsRepo + "/" + this.getWeatherCondition() + "." + imgFormat;
     }
 
     public double getTemperature() {
@@ -22,7 +27,7 @@ public class WeatherData {
         return windSpeed;
     }
 
-    public String getPrecipitation() {
+    public Double getPrecipitation() {
         return precipitation;
     }
 
@@ -33,13 +38,20 @@ public class WeatherData {
     public String getWindDirection() {
         return windDirection;
     }
+    public String getCloudiness() { return cloudiness; }
+    public String getWeatherImgURL() { return weatherConditionImgURL; }
 
+    String imgFormat = "png"; // "png", "svg", "pdf"
+    String weatherIconsRepo = "https://raw.githubusercontent.com/metno/weathericons/main/weather/"
+            + imgFormat;
     double temperature;
     String weatherCondition;
     String windSpeed;
-    String precipitation;
+    Double precipitation;
     String humidity;
     String windDirection;
+    String cloudiness;
+    String weatherConditionImgURL;
 
 }
 
